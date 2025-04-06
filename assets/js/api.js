03848a79ee645b1311e859b6ae7c067ec1059770
Cole Kleinschmidt <ckleinschmidt2@my.stlcc.edu>
2025-04-06 15:48:30 -0500

    More Vercel Tests

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,7 @@
 const ENDPOINT = "https://topx-backend.onrender.com/";
 
+
+//Test Fully Updated
 const createAccountAPI = async (username, email, password) => {
     const data = {
         username: username,
