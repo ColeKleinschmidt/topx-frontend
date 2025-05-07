cb9613cdd72a8affded95f15aca822e17f63dd03
Jacob Olson <jacob.olson@olsonify.com>
2025-05-06 21:41:05 -0500

    formatting nav bar (still looks like poo)

--- src/components/class/NavigationBar.jsx
+++ src/components/class/NavigationBar.jsx
@@ -5,15 +5,30 @@ const NavigationBar = ({ setPage, page }) => {
 
     return (
         <div className="navigation-bar">
-            <div onClick={() => setPage("friends")} className={`navigation-element ${page === "friends" && "underline" }`}>
-                <h2>Friends</h2>
+            <div className={'logo-container'}>
+                <h2>Topx</h2>
             </div>
-            <div onClick={() => setPage("feed")} className={`navigation-element ${page === "feed" && "underline" }`}>
-                <h2>Feed</h2>
+            <div className="navigation-container">
+                <div onClick={() => setPage("friends")} className={`navigation-element ${page === "friends" && "underline" }`}>
+                    <h2>My lists</h2>
+                </div>
+                <div onClick={() => setPage("feed")} className={`navigation-element ${page === "feed" && "underline" }`}>
+                    <h2>Friend lists</h2>
+                </div>
+                <input className="search-lists" placeholder="Search lists" type="text" />
             </div>
-            <div onClick={() => setPage("profile")} className={`navigation-element ${page === "profile" && "underline" }`}>
-                <h2>Profile</h2>
+            <div className={'profile-buttons'}>
+                <div onClick={() => alert("notifications")} className="notifications-button">
+                    alert
+                </div>
+                <div onClick={() => alert("messages")} className="messages-button">
+                    something
+                </div>
+                <div onClick={() => alert("profile")} className="profile-button">
+                something
+                </div>
             </div>
+
         </div>
     )
 }
